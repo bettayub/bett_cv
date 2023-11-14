38cda9dbae1d6fcfff728b17de4f34c9abcd87b1
bettayub <bettayub19@gmail.com>
2023-11-15 00:26:42 +0300

    deploying portfolio

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export default [
+   {
+       title: 'Personal Website',
+       imgUrl: 'assets/portfolio.jpg',
+       stack: ['HTML', 'CSS', 'JavaScript'],
+       link: 'https://bettayub.github.io/my-personal-website/',
+   },
+
+   {
+       title: 'Lost and found frontend',
+       imgUrl: 'assets/logo.jpg',
+       stack: ['Vite', 'Tailwind', 'React'],
+       link: 'https://bett-lost-and-found-frontend.vercel.app/',
+   },
+
+    {
+       title: 'Lost and found backend',
+       imgUrl: 'assets/api.jpg',
+       stack: ['Python', ' Flask-SQLAlchemy', 'Flask-RESTx'],
+       link: 'https://lostandfoundbackend-o0al.onrender.com/',
+   },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      year: 'today',
+      title: 'Freelancer Software Developer',
+      duration: 'to-date',
+      details:
+        'I specialize in freelance software development, collaborating closely with clients and fellow engineers to deliver high-quality, innovative solutions. Proficient in multiple programming languages, I work independently to bring ideas to life, ensuring each project\'s success through tailored, efficient code.',
+    },
+  
+    {
+      year: '2023',
+      title: 'Moringa School',
+      duration: '7 months',
+      details:
+        'Completed the Full Stack Software Development Program at Moringa School, specializing in full-stack development. Proficient in front-end and back-end technologies, with expertise in JavaScript, Java, and Python. Experienced in building responsive and scalable websites using Spring Boot and MySQL databases. Collaborative team player adept at solving complex problems.',
+    },
+  
+    {
+      year: '2018-2022',
+      title: 'Nyandarua University College',
+      duration: '4 years',
+      details:
+        'Studied civil engineering while independently learning coding, gained skills, and contributed to the success of school infrastructure projects by providing innovative ideas and plans. Additionally, while learning coding at my own, I solved engineering problems by collaborating with tech companies.',
+    },
+  
+    {
+      year: '2017',
+      title: 'First Line of Code',
+      duration: '1 year',
+      details:
+        'Joined the Leap Hub Club and attended forums focused on future technologies, emphasizing innovation and solving real-world problems. Excellence was my commitment.',
+    },
+  ];
+  
